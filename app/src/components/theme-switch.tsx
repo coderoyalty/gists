18b004fa2d77f91d7587a04c095a8b3c985fda0c
Akanni Emmanuel <akannie33@gmail.com>
2024-06-08 08:20:47 +0100

    refactor: update theme-switch to use button
    - this update helps improve the space used by the previous switch

--- app/src/components/theme-switch.tsx
+++ app/src/components/theme-switch.tsx
@@ -1,6 +1,7 @@
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import * as Switch from "@radix-ui/react-switch";
 import React from "react";
+import { Button } from "./ui/button";
 
 type ThemeType = "dark" | "light";
 
@@ -23,21 +24,19 @@ export function ThemeSwitch({ render = true }) {
   return (
     <>
       <div className="flex items-center space-x-2">
-        <Switch.Root
-          checked={theme === "dark"}
-          onCheckedChange={() => {
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={() => {
             setTheme((value) => (value === "dark" ? "light" : "dark"));
           }}
-          className="peer inline-flex h-8 w-14 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-slate-900 data-[state=unchecked]:bg-slate-200 dark:focus-visible:ring-slate-300 dark:focus-visible:ring-offset-slate-950 dark:data-[state=checked]:bg-slate-50 dark:data-[state=unchecked]:bg-slate-800"
         >
-          <Switch.Thumb className="pointer-events-none bg-white rounded-full flex justify-center items-center p-[2px] shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0 dark:bg-slate-950">
-            {theme === "dark" ? (
-              <MoonIcon className="w-5 h-5" />
-            ) : (
-              <SunIcon className="w-5 h-5" />
-            )}
-          </Switch.Thumb>
-        </Switch.Root>
+          {theme === "dark" ? (
+            <MoonIcon className="w-5 h-5" />
+          ) : (
+            <SunIcon className="w-5 h-5" />
+          )}
+        </Button>
       </div>
     </>
   );
